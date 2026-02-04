a8bb16ec3e1a440e4400cb7615d2d480828c1596
xibao <1193082625@qq.com>
2026-02-04 15:09:37 +0800

    chore: 【初始化】添加保存自动格式化

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+/**
+ * Prettier 代码格式化配置
+ * 文档：https://prettier.io/docs/en/configuration.html
+ */
+module.exports = {
+  // 是否在语句末尾添加分号
+  semi: false,
+  // 是否使用单引号
+  singleQuote: true,
+  // 设置缩进
+  tabWidth: 2,
+  // 尾随逗号
+  trailingComma: 'es5',
+  // 每行最大字符数
+  printWidth: 120,
+  // 箭头函数参数括号： avoid( 避免 ) | always( 总是 )
+  arrowParens: 'avoid',
+  // 文件行尾： lf( 换行 ) | crlf( 回车换行 ) | auto( 自动 )
+  endOfLine: 'lf',
+}
